0229ebe6c0a0dd038a557974a2cb1a0322f31567
klauslube <klauslube93@gmail.com>
2022-05-18 14:40:00 -0300

    Commit inicial

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 // import user from './user';
 // import wallet from './wallet';
-
+d
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
