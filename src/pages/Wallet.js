a4681e17f220c9250f58f0843ebdbb5782bcea98
klauslube <klauslube93@gmail.com>
2022-05-18 22:29:36 -0300

    Requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,24 +6,71 @@ import { fetchCurrencies } from '../actions/index';
 
 class Wallet extends React.Component {
   componentDidMount() {
-    const { coins } = this.props;
-    coins();
+    const { getCoins } = this.props;
+    getCoins();
   }
 
   render() {
+    const { coins } = this.props;
     return (
-      <div>
+      <>
         <Header />
-      </div>);
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input
+              id="value"
+              type="number"
+              data-testid="value-input"
+              name="value"
+              placeholder="0"
+            />
+          </label>
+          <label htmlFor="coins">
+            Moeda:
+            <select id="coins" name="coins">
+              {coins.map((coin) => <option key={ coin }>{coin}</option>)}
+            </select>
+          </label>
+          <label htmlFor="method">
+            Método de Pagamento:
+            <select id="method" name="method" data-testid="method-input">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="category">
+            Categoria:
+            <select id="catergory" data-testid="tag-input">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+          <label htmlFor="description">
+            Descrição:
+            <input type="text" data-testid="description-input" />
+          </label>
+        </form>
+      </>
+    );
   }
 }
 
+const mapStateToProps = (state) => ({
+  coins: state.wallet.currencies,
+});
+
 const mapDispatchToProps = (dispatch) => ({
-  coins: () => dispatch(fetchCurrencies()),
+  getCoins: () => dispatch(fetchCurrencies()),
 });
 
 Wallet.propTypes = {
-  coins: PropTypes.func.isRequired,
+  getCoins: PropTypes.func.isRequired,
+  coins: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-export default connect(null, mapDispatchToProps)(Wallet);
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
